732c08bb8c99b2d10530fa40645a0b707719a73d
HelLuv <works.person@gmail.com>
2022-01-27 21:23:52 +0300

    set basic style for each component, header markdown added

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from "@material-ui/core";
+import {LocationOnOutlined} from "@material-ui/icons";
+import {Rating} from "@material-ui/lab";
+import useStyles from './style';
 
 interface MapProps {
 
 }
 
-const Map:React.FC<MapProps> = ({})=>{
-
-    return (
-        <h1>Map</h1>
-    )
+const Map: React.FC<MapProps> = ({}) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)')
+  return (
+    <h1>map</h1>
+  )
 };
 
 export default React.memo(Map);
